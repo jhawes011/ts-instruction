6d9f59829f71e9472224d3e93a6a7a0e69aa5baf
Joe Hawes <jhawes011@gmail.com>
2025-03-31 16:37:35 -0400

    class demos

---
+++ ts-demos/ts/functions.ts
@@ -0,0 +1,6 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+let i1: number = 5;
+let i2: number = 10;
+console.log("sum", add(i1, i2)); // sum 15
---
+++ ts-demos/ts/variables.ts
@@ -0,0 +1,23 @@
+let a: number = 5;
+let b: number = 3.14;
+let c: number = a + b;
+console.log("c", c);
+let firstName: string = "John";
+let lastName: string = "Doe";
+let fullName: string = firstName + " " + lastName;
+console.log("fullName", fullName);
+
+let isActive: boolean = true;
+console.log(a === 5); // true
+
+let movie: object = {
+  id: 1,
+  title: "Inception",
+  year: 2010,
+  rating: 8.8,
+};
+console.log(movie);
+
+let numbers: number[] = [0, 1, 2, 3, 4, 5];
+console.log("numbers", numbers);
+numbers.push(6);
