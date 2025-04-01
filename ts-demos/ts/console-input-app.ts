39c961f91daf67938426200795102a5f778f4296
Joe Hawes <jhawes011@gmail.com>
2025-04-01 09:46:50 -0400

    node_module install ts-instruction

---
+++ ts-demos/ts/console-input-app.ts
@@ -0,0 +1,7 @@
+let readline = require("readline-sync");
+
+//get a number
+let nbr1: number = readline.questionInt("Number? ");
+
+// get a string:
+let str1: string = readline.question("String? ");
